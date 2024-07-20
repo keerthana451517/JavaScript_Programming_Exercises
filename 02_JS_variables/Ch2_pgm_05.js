7cf8e484a108e0525c0a32c3de8e9e26eb4245e7
Keerthana <115487278+keerthana451517@users.noreply.github.com>
2024-07-20 18:40:34 +0530

    Ch2_pgm_05.js

--- 02_JS_variables/Ch2_pgm_05.js
+++ 02_JS_variables/Ch2_pgm_05.js
@@ -8,6 +8,12 @@ console.log(message);
 message = 'Congratulations! Your tweet has won a prize...';
 console.log(message);
 
+message = "Hello" + "World!";
+console.log(message);
+
+message = 'Congratulations!' + 'Your tweet' + 'has won a prize...';
+console.log(message);
+
 
 
 /* Further Adventures
