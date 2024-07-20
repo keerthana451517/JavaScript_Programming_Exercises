d836666998fc01c6eaed9837b4055009f85ca52a
Keerthana <115487278+keerthana451517@users.noreply.github.com>
2024-07-20 19:27:40 +0530

    Ch3_pgm_11.js

--- 03_JS_Objects/Ch3_pgm_11.js
+++ 03_JS_Objects/Ch3_pgm_11.js
@@ -1,5 +1,4 @@
 // Using a property in a calculation
-
 var player1;
 
 player1 = {
@@ -13,6 +12,24 @@ player1.score = player1.score + 50;
 
 console.log(player1.name + " has scored " + player1.score);
 
+// Increase player1's score by 10%
+player1.score = player1.score * 1.1;
+console.log(player1.name + " has scored " + player1.score.toFixed(2));
+
+// Add a second player
+var player2 = {
+	  name: "Alex",
+	  score: 0
+};
+
+// Increase player2's score for demonstration
+player2.score = player2.score + 75;
+console.log(player2.name + " has scored " + player2.score);
+
+// Calculate the sum of their scores
+var totalScore = player1.score + player2.score;
+
+console.log(player1.name + " and " + player2.name + " have a total score of " + totalScore.toFixed(2));
 
 
 /* Further Adventures
