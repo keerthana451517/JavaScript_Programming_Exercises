58efbedf7dfd46ba8c7a0cf703a8bdca3f8b09dc
Keerthana <115487278+keerthana451517@users.noreply.github.com>
2024-07-20 19:55:04 +0530

    Ch5_pgm_12.js

--- 05_JS_Arguments/Ch5_pgm_12.js
+++ 05_JS_Arguments/Ch5_pgm_12.js
@@ -1,11 +1,17 @@
 // Displaying a player's location via object properties
-
 var player1;
 var player2;
 var showPlayerPlace;
 
 showPlayerPlace = function (playerName, playerPlace) {
-    console.log(playerName + " is in " + playerPlace);
+    console.log(playerName.substring(0,1) + " is in " + playerPlace);
+    console.log(playerName.substring(0,2) + " is in " + playerPlace);
+    console.log(playerName.substring(0,3) + " is in " + playerPlace);
+    console.log(playerName.substring(1,2) + " is in " + playerPlace);
+    console.log(playerName.substring(1,3) + " is in " + playerPlace);
+
+    //The substring method shows in the array method that is if we take (0,2)
+    //it takes the letters from 0 to 2 but not 2.
 };
 
 player1 = {
@@ -22,7 +28,6 @@ player2 = {
 
 showPlayerPlace(player1.name, player1.place);
 showPlayerPlace(player2.name, player2.place);
-
 
 
 /* Further Adventures
