44ddbc211192294f653318e3da1876904cdd047e
Keerthana <115487278+keerthana451517@users.noreply.github.com>
2024-07-20 19:28:32 +0530

    Ch3_pgm_12.js

--- 03_JS_Objects/Ch3_pgm_12.js
+++ 03_JS_Objects/Ch3_pgm_12.js
@@ -1,4 +1,5 @@
 // A blog post
+const { title } = require("process");
 
 var post = {
 	id : 1,
@@ -7,6 +8,17 @@ var post = {
 	created : "2015-06-21",
 	body : "You will not believe where I just woke up!! Only on a comet..."
 };
+console.log(post.author);
+console.log(post.id);
+console.log(post.title);
+
+var post2 ={
+	id : 2,
+	title : "Personal Bio" ,
+	created : "2024-07-11",
+};
+console.log(post2.id);
+
 
 
 
