225ad666e9fe26abf6344bb1aad944dd85e94b10
Keerthana <115487278+keerthana451517@users.noreply.github.com>
2024-07-20 19:47:33 +0530

    Ch5_pgm_03.js

--- 05_JS_Arguments/Ch5_pgm_03.js
+++ 05_JS_Arguments/Ch5_pgm_03.js
@@ -3,10 +3,12 @@
 var showMessage;
 
 showMessage = function (message) {
-	console.log("The message is: " + message);
+	console.log("The message is: " + message + " Have a pleasant day!");
 };
 
 showMessage("It's full of stars!");
+showMessage("There is a beautiful moon!");
+showMessage("It is awesome to see the plesant night!");
 
 
 
