16e90905f3299c9a9e1ab771d0088079d7e37671
Keerthana <115487278+keerthana451517@users.noreply.github.com>
2024-07-20 19:48:21 +0530

    Ch5_pgm_04.js

--- 05_JS_Arguments/Ch5_pgm_04.js
+++ 05_JS_Arguments/Ch5_pgm_04.js
@@ -1,14 +1,18 @@
 // Calling the same function with different arguments
-
 var showMessage;
 
 showMessage = function (message) {
-	console.log("The message is: " + message);
+	console.log("The message is: ");
+    console.log(message);
 };
 
 showMessage("It's full of stars!");
 showMessage("Hello to Jason Isaacs");
 showMessage("Hello to Jason Isaacs and Stephen Fry");
+
+var myMessage;
+myMessage = "Thank You!";
+showMessage(myMessage);
 
 
 
