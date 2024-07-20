aa97034b0672a3c1d373c588da451c9d3b76a6c9
Keerthana <115487278+keerthana451517@users.noreply.github.com>
2024-07-20 19:31:09 +0530

    Ch3_pgm_15.js

--- 03_JS_Objects/Ch3_pgm_15.js
+++ 03_JS_Objects/Ch3_pgm_15.js
@@ -1,19 +1,31 @@
 // A player object
 
-var player;
+var player1;
 
-player = {
+player1 = {
     name: "Kandra",
     health: 50,
     place: "The Dungeon of Doom",
-    items: "a rusty key, The Sword of Destiny, a piece of cheese"
+    items: ["a rusty key", "The Sword of Destiny", "a piece of cheese"]
 };
 
-console.log(player.name);
-console.log(player.name + " is in " + player.place);
-console.log(player.name + " has health " + player.health);
-console.log("Items: " + player.items);
+console.log(player1.name);
+console.log(player1.name + " is in " + player1.place);
+console.log(player1.name + " has health " + player1.health);
+console.log("Items: " + player1.items.join(", "));
 
+// Create a second player
+var player2 = {
+    name: "Arin",
+    health: 70,
+    place: "The Enchanted Forest",
+    items: ["a healing potion", "a magical bow", "a map"]
+};
+
+console.log(player2.name);
+console.log(player2.name + " is in " + player2.place);
+console.log(player2.name + " has health " + player2.health);
+console.log("Items: " + player2.items.join(", "));
 
 
 /* Further Adventures
