96f0f1fccb117caf6c9ed1753d95d74b747656ff
Keerthana <115487278+keerthana451517@users.noreply.github.com>
2024-07-20 20:08:58 +0530

    Ch7_pgm_03.js

--- 07_JS_Object_Arguments/Ch7_pgm_03.js
+++ 07_JS_Object_Arguments/Ch7_pgm_03.js
@@ -1,5 +1,4 @@
 // A function to create planets
-
 var buildPlanet;
 var getPlanetInfo;
 var planet1;
@@ -24,6 +23,32 @@ planet2 = buildPlanet("Neptune", 8, "Ice Giant", 24622, 4);
 
 console.log(getPlanetInfo(planet1));
 console.log(getPlanetInfo(planet2));
+
+// For cars
+var buildCar;
+var getCarInfo;
+var car1;
+var car2;
+
+buildCar = function (name, type, kilometer, country) {
+    return {
+        name: name,
+        type: type,
+        kilometer:kilometer,
+        country:country
+    };
+};
+
+getCarInfo = function (car) {
+    return car.name.toUpperCase() + ": car " + car.type;
+};
+
+car1 = buildCar("VolksWagon", "Taigun", 140, "USA");
+car2 = buildCar("Tata", "Harrier", 170, "India");
+
+console.log(getCarInfo(car1));
+console.log(getCarInfo(car2));
+
 
 
 
