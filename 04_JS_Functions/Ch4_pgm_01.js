81f1f3e842ffb49c957e2675518793c0c5ac6fa7
Keerthana <115487278+keerthana451517@users.noreply.github.com>
2024-07-20 19:33:24 +0530

    Ch4_pgm_01.js

--- 04_JS_Functions/Ch4_pgm_01.js
+++ 04_JS_Functions/Ch4_pgm_01.js
@@ -14,6 +14,40 @@ console.log("Actors: " + movie1.actors);
 console.log("Directors: " + movie1.directors);
 console.log("------------------------------");
 
+//second movie
+var movie2;
+movie2 ={
+  title : "Indian2",
+  actors : "Kamal Hasan",
+  directors : "S. Shankar"
+};
+
+console.log("Movie information for " + movie2.title);
+console.log("------------------------------");
+console.log("Actors: " + movie2.actors);
+console.log("Directors: " + movie2.directors);
+console.log("------------------------------");
+
+//blog post
+
+// Create an object to represent a blog post
+var blogPost;
+
+blogPost = {
+  title: "Exploring JavaScript",
+  author: "Dhaanush S B",
+  date: "July 12, 2024",
+  content: "JavaScript is a versatile programming language used for web development."
+};
+
+// Display information about the blog post
+console.log("Blog Post Information");
+console.log("------------------------------");
+console.log("Title: " + blogPost.title);
+console.log("Author: " + blogPost.author);
+console.log("Date: " + blogPost.date);
+console.log("Content: " + blogPost.content);
+console.log("------------------------------");
 
 
 /* Further Adventures
