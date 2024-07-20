7cd36ad0da476940820290dcede10c105e5424fa
Keerthana <115487278+keerthana451517@users.noreply.github.com>
2024-07-20 20:15:09 +0530

    Ch7_pgm_08.js

--- 07_JS_Object_Arguments/Ch7_pgm_08.js
+++ 07_JS_Object_Arguments/Ch7_pgm_08.js
@@ -3,6 +3,12 @@
 var message = "We choose to go to the Moon!";
 
 console.log(message.substr(3, 12));
+console.log(message.substr(20)); // Logs "Moon!"
+console.log(message.substr(11, 6)); // Logs "go to the"
+console.log(message.substr(0, 10));
+console.log(message.substr(3)); // Logs "choose to go to the Moon!"
+console.log(message.substr(-5)); // Logs "Moon!"
+console.log(message.substr(11, -3)); // Logs "go to the"
 
 
 
